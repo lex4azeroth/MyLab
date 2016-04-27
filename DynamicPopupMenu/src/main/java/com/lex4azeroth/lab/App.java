4cc722f339fc5d658c06ba3217268892b52777f0
lex4azeroth <mxhray@hotmail.com>
2016-04-27 22:04:04 +0800

    init checkin

---
+++ DynamicPopupMenu/src/main/java/com/lex4azeroth/lab/App.java
@@ -0,0 +1,13 @@
+package com.lex4azeroth.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
